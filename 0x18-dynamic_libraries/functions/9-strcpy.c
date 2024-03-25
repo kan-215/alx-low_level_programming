2415c6e89a8faca64f5fec33a07cffbeed7b0556
kan-215 <katamialvin369@gmail.com>
2024-03-26 01:01:39 +0300

    libdynamics.so

--- 0x18-dynamic_libraries/functions/9-strcpy.c
+++ 0x18-dynamic_libraries/functions/9-strcpy.c
@@ -8,20 +8,20 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-    int i = 0;
-    int j = 0;
+int i = 0;
+int j = 0;
 
-    while (*(src + i) != '\0')
-    {
-        i++;
-    }
+while (*(src + i) != '\0')
+{
+i++;
+}
 
-    for (; j < i; j++)
-    {
-        dest[j] = src[j];
-    }
+for (; j < i; j++)
+{
+dest[j] = src[j];
+}
 
-    dest[i] = '\0';
+dest[i] = '\0';
 
-    return (dest);
+return (dest);
 }
